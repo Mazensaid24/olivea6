abaa2a677692d37f2fb93ea3939adbcb63853f17
Mazen Said <155029461+Mazensaid24@users.noreply.github.com>
2024-05-04 23:51:28 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["Ve_G4"]
+OWNER_NAME = "Mazen"
+BOT_TOKEN = "6615977391:AAH0fFJQZ_gIhyHOiTK1FQc-gZjI7rGRCNs"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/Source_Katya"
+GROUP = "https://t.me/miika3"
+VIDEO = "https://telegra.ph/file/af778e71176ac74a8fd00.jpg"
+LOGS = "zombie2"
+
---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+from OWNER import BOT_TOKEN, OWNER, OWNER_NAME, DATABASE, CHANNEL, GROUP, LOGS, VIDEO
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv()
+admins = {}
+user = {}
+call = {}
+dev = {}
+logger = {}
+logger_mode = {}
+botname = {}
+appp = {}
+helper = {}
+
+
+
+API_ID = int(getenv("API_ID", "22861564"))
+API_HASH = getenv("API_HASH", "d21d3241e065ab83a101b10fd750201b")
+BOT_TOKEN = BOT_TOKEN
+MONGO_DB_URL = DATABASE
+OWNER = OWNER
+OWNER_NAME = OWNER_NAME
+CHANNEL = CHANNEL
+GROUP = GROUP
+LOGS = LOGS
+VIDEO = VIDEO
